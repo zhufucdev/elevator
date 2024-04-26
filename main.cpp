2643a9e0e4cd66fa8f8fd8136fac72a7284500f6
zhufucdev <1095291434@qq.com>
2024-04-26 23:36:23 +0800

    :memo: add documentation

--- main.cpp
+++ main.cpp
@@ -6,11 +6,26 @@ using namespace std;
 
 const int I = 0, __SZ = 10;
 
+/**
+ * 表示每楼层外面板上的一个按钮
+ */
 struct Button
 {
+    /**
+     * 向上的按钮是否按下
+     */
     bool up;
+    /**
+     * 向下的按钮是否按下
+     */
     bool down;
+    /**
+     * 想向上的人数
+     */
     int upCnt = 0;
+    /**
+     * 想向下的人数
+     */
     int downCnt = 0;
 
     Button() {}
@@ -18,12 +33,31 @@ struct Button
             : up(u), down(d), upCnt(iu), downCnt(id) {}
 };
 
+/**
+ * 表示一个矫箱
+ */
 struct Elevator
 {
+    /**
+     * 表示是否有人
+     */
     int status;
+    /**
+     * 所在楼层
+     */
     int level;
+    /**
+     * 自动移动方向，0向下，1向上
+     */
     int autoModeFace; // 0 -> down, 1 -> up
+    /**
+     * 电梯是否满员
+     */
     bool isFull;
+    /**
+     * 矫箱内按钮是否按下。
+     * 当btn[x] == y时，去y楼的按钮被按下
+     */
     int btn[__SZ];
 
     Elevator() {}
@@ -40,7 +74,7 @@ struct elevatorGame
 
     elevatorGame() {}
 
-    void __newGame()
+    void newGame()
     {
         roundId += 1;
         for (int i = 0; i < __SZ; i++)
@@ -49,59 +83,30 @@ struct elevatorGame
             elevators[i] = Elevator(I, 0, 0, 0, false);
     }
 
-    void __getInfo()
+    void getInfo()
     {
-        string __buttons, __a, __b, __c, __d;
+        string directions, persons_up, persons_down, is_full, is_pressed;
         for (int i = 1; i <= 6; i++)
         {
-            cin >> __buttons >> __a >> __b;
-            buttons[i] = Button(__buttons[0] == '1', __buttons[1] == '1', stoi(__a), stoi(__b));
+            cin >> directions >> persons_up >> persons_down;
+            buttons[i] = Button(directions[0] == '1', directions[1] == '1', stoi(persons_up), stoi(persons_down));
         }
 
         for (int i = 1; i <= 5; i++)
         {
-            cin >> __a >> __b >> __c >> __d;
-            int a = __a[0] == '1', b = stoi(__b);
-            int c = __a[1] == '1', d = stoi(__c);
-            c = a ? c : 2;
-            elevators[i] = Elevator(I, a, b, c, d);
-            for (size_t p = 0; p < __d.size(); p++)
-                elevators[i].btn[p + 1] = __d[p] == '1';
+            cin >> persons_up >> persons_down >> is_full >> is_pressed;
+            int contains_rider = persons_up[0] == '1', level = stoi(persons_down);
+            int auto_face_mode = persons_up[1] == '1', is_full_int = stoi(is_full);
+            auto_face_mode = contains_rider ? auto_face_mode : 2; // 这啥鸡巴意思
+            elevators[i] = Elevator(I, contains_rider, level, auto_face_mode, is_full_int);
+            for (size_t p = 0; p < is_pressed.size(); p++)
+                elevators[i].btn[p + 1] = is_pressed[p] == '1';
         }
     }
 } GAME;
 
 string yourTurn()
 {
-    /********************修改本函数，以下为示例***************/
-    string res(" UUUUU");
-
-    int bid = 1;
-    buttons[bid].down;    // bool 这层是否有人希望向下
-    buttons[bid].up;      // bool 这层是否有人希望向上
-    buttons[bid].upCnt;   // int  这层有多少人希望向上
-    buttons[bid].downCnt; // itn  这层有多少人希望向下
-
-    int eid = 1;
-    elevators[eid].status;       // 这台电梯里有没有人
-    elevators[eid].level;        // 这台电梯的楼层
-    elevators[eid].autoModeFace; // 这台电梯的移动方向
-    elevators[eid].isFull;       // 这台电梯是否满员
-
-    elevators[eid].btn[1]; // bool 这台电梯是否有人前往 1 层
-    elevators[eid].btn[2]; // bool 这台电梯是否有人前往 2 层
-    // ...
-    elevators[eid].btn[6]; // bool 这台电梯是否有人前往 6 层
-
-    int i = 1;
-    // 让第 i 台电梯向上
-    res[i] = 'U';
-    // 让第 i 台电梯向下
-    res[i] = 'D';
-    // 让第 i 台电梯停留
-    res[i] = 'S';
-
-    return res;
 }
 
 int main()
@@ -111,8 +116,8 @@ int main()
         string first;
         cin >> first;
         if (first == "N")
-            GAME.__newGame();
-        GAME.__getInfo();
+            GAME.newGame();
+        GAME.getInfo();
         string str = yourTurn();
         transform(str.begin(), str.end(), str.begin(), [](unsigned char c)
         { return toupper(c); });
