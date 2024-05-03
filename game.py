cd8d8490664ab180ae1e73784263cad88f8e4bcf
zhufucdev <1095291434@qq.com>
2024-05-03 09:19:03 +0800

    :sparkles: graphic frontend

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import math
-import time
+import pygame
+from pygame.locals import *
 from collections import deque
 from argparse import ArgumentParser
 from enum import Enum
@@ -57,7 +58,7 @@ class Carriage:
     def __take_passengers(self):
         target = self.__game.floors[self.floor]
         free_space = self.capacity - self.riders
-        while free_space > 0:
+        while free_space > 0 and len(target.passengers) > 0:
             comer = target.passengers.popleft()
             self.passengers.append(comer)
             comer.land(self)
@@ -80,7 +81,7 @@ class Carriage:
                 self.floor = 0 if self.floor < 0 else len(self.__game.floors) - 1
                 self.state = CarriageState.FORCE_AUTONOMA
         elif self.empty and self.state == CarriageState.FORCE_AUTONOMA:
-            targetable_floors = filter(lambda f: f.waiting > 0, self.__game.floors)
+            targetable_floors = list(filter(lambda f: f.waiting > 0, self.__game.floors))
             self.__take_passengers()
             if len(targetable_floors) > 0:
                 target = min(targetable_floors, key=lambda f: abs(f.num - self.num))
@@ -125,7 +126,10 @@ class Floor:
         if comers > 0:
             self.__luck = 0
             for i in range(comers):
-                self.passengers.append(Passenger(math.ceil((random() * len(self.__game.floors)))))
+                go = math.ceil((random() * len(self.__game.floors)))
+                while go == self.num:
+                    go = math.ceil((random() * len(self.__game.floors)))
+                self.passengers.append(Passenger(go))
 
 
 class Scheduler:
@@ -134,6 +138,17 @@ class Scheduler:
 
     def tick(self):
         pass
+
+
+class FullAutonomaScheduler(Scheduler):
+    def __init__(self, game: 'Game'):
+        super().__init__(game)
+
+    def tick(self):
+        for car in self._game.cars:
+            if car.state == CarriageState.IDLE:
+                car.state = CarriageState.FORCE_AUTONOMA
+            print(car.num, car.floor)
 
 
 class StdIOScheduler(Scheduler):
@@ -172,18 +187,101 @@ class StdIOScheduler(Scheduler):
             car.state = CarriageState.IDLE if sc[i] == 'S' else CarriageState.UP if sc[i] == 'U' else CarriageState.DOWN
 
 
+SEPARATOR_X = 200
+LINE_WIDTH = 8
+BG_COLOR = 0xffffff
+LINE_COLOR = 0x0
+FLOOR_COLORS = [0xf1184c, 0xfffc40, 0xfaba61, 0xff8172, 0x006fff, 0x3a579a, 0x36244f]
+PASSENGER_RADIUS = 10
+CAR_COLOR = 0x666666
+
+
 class Game:
+    __display: pygame.Surface
+    __font: pygame.font.Font
+
     def __init__(self, cars: int, floors: int, scheduler: type[Scheduler], **kwargs):
         self.floors = list(Floor(i, self, math.ceil(math.e ** (3 - i))) for i in range(floors))
         self.cars = list(Carriage(i, self) for i in range(cars))
         self.scheduler = scheduler(game=self, **kwargs)
 
-    def tick(self):
+        self.fps = 0
+        self.size = self.width, self.height = 1280, 720
+        self.running = False
+        self.tick_iter = 0
+        self.speed = 2
+
+    def init(self):
+        pygame.init()
+        self.__display = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.__font = pygame.font.SysFont(size=32, name=pygame.font.get_default_font())
+        self.running = True
+
+    def run_blocking(self, fps: int = 60):
+        self.fps = fps
+        clock = pygame.time.Clock()
+        frame = 0
+        while self.running:
+            for event in pygame.event.get():
+                self.__on_event(event)
+            self.__update()
+            pygame.display.update()
+            if frame % (fps / (self.speed + 1)) == 0:
+                self.__tick()
+            clock.tick(fps)
+            frame += 1
+
+    def __tick(self):
         self.scheduler.tick()
         for car in self.cars:
             car.tick()
         for floor in self.floors:
             floor.tick()
+
+    def __update(self):
+        dp = self.__display
+        dp.fill(BG_COLOR)
+
+        dp.fill(LINE_COLOR, (SEPARATOR_X, 0, LINE_WIDTH, self.height))
+        floor_height = self.height / len(self.floors)
+        car_height = floor_height
+        car_width = (self.width - SEPARATOR_X - LINE_WIDTH) / len(self.cars)
+        for i, floor in enumerate(self.floors):
+            floor_y = self.height - floor_height * i
+            dp.fill(LINE_COLOR, (0, floor_y, SEPARATOR_X, LINE_WIDTH))
+            dp.fill(FLOOR_COLORS[i], (0, floor_y - floor_height + LINE_WIDTH, 20, 20))
+            if len(floor.passengers) > 0:
+                passenger_width = SEPARATOR_X / len(floor.passengers)
+                for j, passenger in enumerate(floor.passengers):
+                    pygame.draw.circle(dp, FLOOR_COLORS[passenger.go],
+                                       (SEPARATOR_X - PASSENGER_RADIUS - passenger_width * j,
+                                        floor_y - PASSENGER_RADIUS),
+                                       PASSENGER_RADIUS)
+
+        for i, car in enumerate(self.cars):
+            car_x = SEPARATOR_X + car_width * i + LINE_WIDTH * 1.5
+            car_y = self.height - car_height * (car.floor + 1)
+            pygame.draw.rect(dp, CAR_COLOR, (car_x, car_y, car_width - 2 * LINE_WIDTH, car_height), LINE_WIDTH)
+
+            if len(car.passengers) > 0:
+                passenger_width = (car_width - 2 * LINE_WIDTH) / len(car.passengers)
+                for j, passenger in enumerate(car.passengers):
+                    pygame.draw.circle(dp, FLOOR_COLORS[passenger.go],
+                                       (car_x + passenger_width * j, car_y - car_height - PASSENGER_RADIUS),
+                                       PASSENGER_RADIUS)
+
+        pygame.display.set_caption(f'speed {self.speed + 1}')
+
+    def __on_event(self, event: pygame.event.Event):
+        if event.type == pygame.QUIT:
+            self.running = False
+        elif event.type == pygame.KEYUP:
+            code = event.dict['key']
+            print(code)
+            if code == 61:
+                self.speed = (self.speed + 1) % self.fps
+            elif code == 45:
+                self.speed = (self.speed - 1) % self.fps
 
     @property
     def unhappiness(self):
@@ -191,10 +289,10 @@ class Game:
 
 
 def main(executable: str):
-    game = Game(cars=5, floors=6, scheduler=StdIOScheduler, executable=executable)
-    for i in range(600):
-        print(f'tick#{i}: {" ".join(str(len(f.passengers)) for f in game.floors)}')
-        game.tick()
+    # game = Game(cars=5, floors=6, scheduler=StdIOScheduler, executable=executable)
+    game = Game(cars=5, floors=6, scheduler=FullAutonomaScheduler)
+    game.init()
+    game.run_blocking()
 
     print(f'unhappiness: {game.unhappiness}')
 
